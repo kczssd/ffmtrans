d4cce6d239248c4dd969c64d3c3920c704872449
kczssd <511124893@qq.com>
2023-05-07 12:23:04 +0800

    add: prefilght

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,19 @@
+use std::io;
+
 use actix_cors::Cors;
-use actix_web::{web, App, HttpServer};
+use actix_web::{http, middleware, web, App, HttpResponse, HttpServer};
 use ffmtrans::serve::route::{trans_handler, ThreadChannel};
+
+async fn preflight() -> io::Result<HttpResponse> {
+    Ok(HttpResponse::Ok()
+        .append_header(("Access-Control-Allow-Origin", "*"))
+        .append_header(("Access-Control-Allow-Methods", "POST, OPTIONS"))
+        .append_header((
+            "Access-Control-Allow-Headers",
+            "Content-Type, x-requested-with",
+        ))
+        .finish())
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -9,9 +22,16 @@ async fn main() -> std::io::Result<()> {
     // route init
     HttpServer::new(move || {
         App::new()
-            .wrap(Cors::default().allow_any_origin())
+            .wrap(
+                Cors::default()
+                    .allow_any_origin()
+                    .allowed_methods(vec!["POST", "OPTIONS"])
+                    .allow_any_header()
+                    .max_age(3600),
+            )
             .app_data(thread_channel.clone())
             .route("/setosd", web::post().to(trans_handler))
+            .route("/setosd", web::method(http::Method::OPTIONS).to(preflight))
     })
     .bind(("127.0.0.1", 3000))?
     .run()
